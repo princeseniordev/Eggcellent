ceee9a4b62b54a8f986babdb9f4e9da44f41ac3e
princeseniordev <princeseniordev@gmail.com>
2023-05-24 21:07:49 +0300

    responsive design

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,53 +10,76 @@ import paper from './../../assets/images/paper-logo-icon.svg';
 const Header: FC = () => {
 
 	const [scrollPosition, setScrollPosition] = useState(0);
+	const [width, setWidth] = useState(window.innerWidth);
 
 	useEffect(() => {
+        const onSize = () => setWidth(window.innerWidth);;
         const onScroll = () => setScrollPosition(window.pageYOffset);
         window.removeEventListener('scroll', onScroll);
         window.addEventListener('scroll', onScroll, { passive: true });
-		console.log("scrollposition", scrollPosition);
+        window.removeEventListener('resize', onSize);
+        window.addEventListener('resize', onSize, { passive: true });
         return () => window.removeEventListener('scroll', onScroll);
     });
 
 	const [showNav, setShowNav] = useState(false);
 
 	const navItems = [
-		<a href="#main" onClick={() => setShowNav(false)}> Dashboard </a>,
 		<a href="#home" onClick={() => setShowNav(false)}> Home </a>,
 		<a href="#documentation" onClick={() => setShowNav(false)}> Documentation </a>,
 		<a href="#roadmap" onClick={() => setShowNav(false)}> Roadmap </a>,
 		<a href="#team" onClick={() => setShowNav(false)}> Team </a>,
 		<a href="#contact" onClick={() => setShowNav(false)}> Contact </a>,
-		<a href="#home" style={{ display: 'flex', alignItems: 'cemter' }}><i style={{ backgroundImage: `url(${metamask})`, width: '26px', height: '26px', marginRight: '7px' }}></i>Mint</a>,
+		<a href="#home" style={{ display: 'flex', alignItems: 'center' }}><i style={{ backgroundImage: `url(${metamask})`, width: '26px', height: '26px', marginRight: '7px' }}></i>Mint</a>,
 		<a href="#home" style={{ display: 'flex', alignItems: 'center' }}><i style={{ backgroundImage: `url(${paper})`, width: '26px', height: '26px', marginRight: '7px' }}></i>Mint</a>
 	];
 
-	const close = [<span onClick={() => setShowNav(false)} style={{ cursor: 'pointer' }}> &times; </span>];
+	const close = [<span onClick={() => setShowNav(false)} style={{ cursor: 'pointer' }}>&times;</span>];
 	
 	return (
-		<div 
-			className={cn([
-				styles['kut-header'],
-                scrollPosition > 400 ? styles['kut-header-white'] : '',
-			])}
-		>
-			<img src={logo} alt="logo" />
-			<div className={`${styles['openNav']}`}  onClick={() => setShowNav(true)}>
-				<MenuIcon/>
-			</div>
-			<SideNav
-				showNav={showNav}
-				openFromRight={true}
-				onHideNav={() => setShowNav(false)}
-				style={{'zIndex': 5000}}
-				title={close}
-				titleStyle={{ backgroundColor: '#8459FF', 'lineHeight': 0, padding: '40px', 'textAlign': 'right'}}
-				items={navItems}
-				itemStyle={{ backgroundColor: '#8459FF' }}
-				itemHoverStyle={{ backgroundColor: '#CDDC39' }}
-			/>
-		</div>
+		<>
+			{
+				(scrollPosition > 400 && width >= 1024)
+				? 
+				<div className={`${styles['header']}`}>
+					<ul>
+						<li><img src={logo} alt="logo"/></li>
+						<div>
+							<li><a href="#home">Home</a></li>
+							<li><a href="#documentation">Documentation</a></li>
+							<li><a href="#roadmap">Roadmap</a></li>
+							<li><a href="#team">Team</a></li>
+							<li><a href="#contact">Contact</a></li>
+							<li className={`${styles['btn-content']}`}><button className={`${styles['metamask']}`}><i></i>Mint</button></li>
+							<li className={`${styles['btn-content']}`}><button className={`${styles['paper']}`}><i></i>Mint</button></li>
+						</div>
+					</ul>
+				</div>
+				:
+				<div 
+					className={cn([
+						styles['kut-header'],
+						scrollPosition > 400 ? styles['kut-header-white'] : '',
+					])}
+				>
+					<img src={logo} alt="logo" />
+					<div className={`${styles['openNav']}`}  onClick={() => setShowNav(true)}>
+						<MenuIcon/>
+					</div>
+					<SideNav
+						showNav={showNav}
+						openFromRight={true}
+						onHideNav={() => setShowNav(false)}
+						style={{'zIndex': 5000}}
+						title={close}
+						titleStyle={{ backgroundColor: '#8459FF', 'lineHeight': 0, padding: '40px', 'textAlign': 'right'}}
+						items={navItems}
+						itemStyle={{ backgroundColor: '#8459FF' }}
+						itemHoverStyle={{ backgroundColor: '#CDDC39' }}
+					/>
+				</div>
+			}
+		</>
 	)
 };
 
--- src/pages/IntroducePage/IntroducePage.tsx
+++ src/pages/IntroducePage/IntroducePage.tsx
@@ -12,7 +12,6 @@ import chick1 from './../../assets/images/chick1.svg';
 import chick2 from './../../assets/images/chick2.svg';
 import chick3 from './../../assets/images/chick3.svg';
 import chick4 from './../../assets/images/chick4.svg';
-import polygon from './../../assets/images/polygon.png';
 
 interface ComProps {
 	children?: any;
@@ -29,20 +28,19 @@ const IntroducePage: FC<ComProps> = ({
 						<video className={`${styles['video']}`} autoPlay playsInline muted loop preload="" poster="./../../assets/video/rainbow-circle-black.25ba983.jpg">
 							<source src={require('./../../assets/video/rainbow1.mp4')} type="video/mp4"/>
 						</video>
-						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 750 100" preserveAspectRatio="xMidYMid slice">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 750 200" preserveAspectRatio="xMidYMid slice">
 							<defs>
 								<mask id="maskblack" x="0" y="0" width="100%" height="100%">
 									<rect x="0" y="0" width="100%" height="100%"></rect>
-									<text x="50%" y="50%" dominantBaseline="middle" textAnchor="middle"> EGGCELLENT NFT CLUB </text>
+									<text x="50%" y="50%" dominantBaseline="middle" textAnchor="middle"> EGGCELENT </text>
 								</mask>
 							</defs>
 							<rect className={`${styles['rect']}`} x="0" y="0" width="100%" height="100%"></rect>
 						</svg>
 					</div>
-					<p className={`${styles['heading-sm']}`}> Backed by Real-World Chicken Eggs Farming Business </p>
-					<img src={polygon} alt="polygon" className={`${styles['polygon']}`}/>
+					<p className={`${styles['heading-sm']}`}>Net Club</p>
 				</div>
-				<Link to="/eggcellent" className={`${styles['btn-start']}`}><button> LET'S EXPLORE </button></Link>
+				<Link to="/eggcellent"><button className={`${styles['btn-start']}`}> LET'S EXPLORE </button></Link>
 				<div className={`${styles['aqua-ball']} ${styles['animation-1']} ${styles['ball-animation']}`}>
 					<img src={aquaBall} alt="" />
 				</div>
@@ -64,7 +62,7 @@ const IntroducePage: FC<ComProps> = ({
 				<div className={`${styles['chick1']} ${styles['animation-2']} ${styles['ball-animation']}`}>
 					<img src={chick1} alt="" />
 				</div>
-				<div className={`${styles['chick2']} ${styles['animation-1']} ${styles['ball-animation']}`}>
+				<div className={`${styles['chick2']} ${styles['animation-2']} ${styles['ball-animation']}`}>
 					<img src={chick2} alt="" />
 				</div>
 				<div className={`${styles['chick3']} ${styles['animation-1']} ${styles['ball-animation']}`}>
